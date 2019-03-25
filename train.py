082db079fade4c7cf2c902eb1626c79bf0828236
shangeth <shangethrajaa@gmail.com>
2019-03-25 17:03:14 +0530

    save model error fixed

--- train.py
+++ train.py
@@ -127,7 +127,7 @@ def main():
 	hidden_size = 256
 	encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
 	attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
-	trainIters(encoder1, attn_decoder1, int(args.epochs), pairs, print_every=5000, float(args.lr))
+	trainIters(encoder1, attn_decoder1, int(args.epochs), pairs, print_every=5000, learning_rate=float(args.lr))
 
 
 
